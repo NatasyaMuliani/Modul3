3cdfc9e15c110f18760484a651b60b5c684838c3
mellysafitri <mellysafitri056@gmail.com>
2022-03-28 14:54:06 +0700

    String Replace

---
+++ src/_14_StringReplace.java
@@ -0,0 +1,7 @@
+public class _14_StringReplace {
+    public static void main(String[] args) {
+        String nama="Dora";
+        nama = nama.replace("Dor","Ti");
+        System.out.println(nama);
+    }
+}
